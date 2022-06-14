4c1bb1c495a4b82fd81f54464d3ac8c9a783b6c7
erikasemana <sempanaerika@gmail.com>
2022-06-14 21:41:31 +0700

    add Profile Page

---
+++ src/screen/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+// import styles from './styles';
+
+export default function Profile(props) {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
